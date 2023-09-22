e3a461428408b6ef841ffa2172b82ec4ef04a54e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-09-22 09:03:48 +0000

    ok

--- leftRotateArrayByDposition.java
+++ leftRotateArrayByDposition.java
@@ -5,10 +5,10 @@ public class leftRotateArrayByDposition {
             temp[i]=arr[i];
         }
         for(int i=d;i<arr.length;i++){
-            arr[i-3]=arr[i];     //shifting the elements according to d 
+            arr[i-4]=arr[i];     //shifting the elements according to d 
         }
         int j=0;
-        for(int i=d+1;i<arr.length;i++){
+        for(int i=d-1;i<arr.length;i++){
             arr[i]=temp[j];
             j++;
         }
@@ -18,6 +18,6 @@ public class leftRotateArrayByDposition {
     }
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6,7};
-        rotate(arr, 3);
+        rotate(arr, 4);
     }
 }
