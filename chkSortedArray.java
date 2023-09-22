21db714b3357f8b6e75c26d6ecc6976a03f2aebe
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-09-22 05:21:09 +0000

    check array sorted or not

---
+++ chkSortedArray.java
@@ -0,0 +1,13 @@
+public class chkSortedArray {
+    static boolean checkSort(int[] a,int n){
+        for(int i=0;i<n-1;i++){
+            if(a[i]>a[i+1])
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr={2,3,4,5,20};
+        System.out.println(checkSort(arr, 5));
+    }
+}
