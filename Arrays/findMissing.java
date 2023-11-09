6e71345bc17b3134ab461f3b4c366a9c59d18c32
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-09 07:18:50 +0000

    find missing number in array

---
+++ Arrays/findMissing.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class findMissing {
+    static int getMissing(int[] arr, int n) {
+        int totalSum = (n * (n + 1)) / 2;
+        int arrSum = 0;
+        for (int x : arr)
+            arrSum += x;
+
+        return totalSum - arrSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 4, 5, 6 };
+        System.out.println(getMissing(arr, arr.length));
+    }
+}
