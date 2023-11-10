555f0d4dd625e263da22f93dc482758ca0543962
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-10 18:07:07 +0000

    100% test cases passed

--- Arrays/numAppearingOnce.java
+++ Arrays/numAppearingOnce.java
@@ -4,19 +4,18 @@ public class numAppearingOnce {
     static int getNum(int[] arr, int n) {
         for (int i = 0; i < n; i++) {
             int count = 0;
-            int key = arr[i];
             for (int j = 0; j < n; j++) {
-                if (arr[j] == key)
+                if (arr[j] == arr[i])
                     count++;
             }
             if (count == 1)
-                return key;
+                return arr[i];
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 8, 8, 8, 9, 9, 6, 6, 2, 2, 0 };
+        int[] arr = { 8, 8, 8, 9, 9, 6, 2, 2, 0,0};
         System.out.println(getNum(arr, arr.length));
     }
 }
