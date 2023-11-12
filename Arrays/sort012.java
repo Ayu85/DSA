45e87b0ad79a4860ba88325efc5299098b130ea9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-12 17:54:07 +0000

    dsa revision

--- Arrays/sort012.java
+++ Arrays/sort012.java
@@ -29,7 +29,7 @@ public class sort012 {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 2, 2, 1, 1, 0, 2, 1, 2, 0, 1, 2 };
+        int[] arr = { 2, 2, 1, 1, 0, 2, 1, 2, 0 };
         sortAll(arr, arr.length);
         printArr(arr);
 }
